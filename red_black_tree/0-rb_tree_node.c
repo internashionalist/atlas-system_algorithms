42259c31a839461506ae88801347f0e77400a833
Nash Thames <taylor.thames@atlasschool.com>
2025-07-03 07:02:50 -0500

    betty

--- red_black_tree/0-rb_tree_node.c
+++ red_black_tree/0-rb_tree_node.c
@@ -10,17 +10,17 @@
  */
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
-    rb_tree_t *new;					/* pointer to new node */
+	rb_tree_t *new;						/* pointer to new node */
 
 	new = malloc(sizeof(rb_tree_t));	/* allocate its memory */
 	if (new == NULL)					/* failure check */
-        return (NULL);
+		return (NULL);
 
-    new->n = value;					/* set node value */
-    new->color = color;				/* set node color */
-    new->parent = parent;				/* set parent pointer */
-    new->left = NULL;					/* set left child pointer */
-    new->right = NULL;					/* set right child pointer */
+	new->n = value;						/* set node value */
+	new->color = color;					/* set node color */
+	new->parent = parent;				/* set parent pointer */
+	new->left = NULL;					/* set left child pointer */
+	new->right = NULL;					/* set right child pointer */
 
-    return (new);						/* return pointer to new node */
+	return (new);						/* return pointer to new node */
 }
