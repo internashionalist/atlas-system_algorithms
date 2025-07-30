a6d9772330b8433417c438d4ba88d5522e00bb78
Nash Thames <taylor.thames@atlasschool.com>
2025-07-30 02:51:05 -0500

    betty

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -11,18 +11,18 @@
  */
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
-    heap_t *heap;						/* ptr to heap structure */
+	heap_t *heap;						/* ptr to heap structure */
 
-    if (data_cmp == NULL)				/* NULL check */
-        return (NULL);
+	if (data_cmp == NULL)				/* NULL check */
+		return (NULL);
 
-    heap = malloc(sizeof(heap_t));		/* heap memory allocation */
-    if (heap == NULL)
-        return (NULL);
+	heap = malloc(sizeof(heap_t));		/* heap memory allocation */
+	if (heap == NULL)
+		return (NULL);
 
-    heap->root = NULL;					/* init root to NULL */
-    heap->size = 0;						/* init size to 0 */
-    heap->data_cmp = data_cmp;			/* assign comparison function */
+	heap->root = NULL;					/* init root to NULL */
+	heap->size = 0;						/* init size to 0 */
+	heap->data_cmp = data_cmp;			/* assign comparison function */
 
-    return (heap);						/* return newly created heap */
+	return (heap);						/* return newly created heap */
 }
