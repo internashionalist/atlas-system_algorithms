e7db3564e3f9fed27aec574630b626b99d78a248
Nash Thames <taylor.thames@atlasschool.com>
2025-08-01 14:36:20 -0500

    symbols are weird

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -16,11 +16,6 @@ typedef struct symbol_s
 } symbol_t;
 
 /* PROTOTYPES */
-heap_t *heap_create(int (*data_cmp)(void *, void *));
-binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
-binary_tree_node_t *heap_insert(heap_t *heap, void *data);
-void *heap_extract(heap_t *heap);
-void heap_delete(heap_t *heap, void (*free_data)(void *));
 symbol_t *symbol_create(char data, size_t freq);
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 int huffman_extract_and_insert(heap_t *priority_queue);
---
+++ huffman_coding/symbol.c
@@ -0,0 +1,28 @@
+#include "huffman.h"
+
+/**
+ * symbol_create -	creates a symbol_t structure
+ * @data:			character to store in the symbol
+ * @freq:			frequency associated with the symbol
+ *
+ * Return:			pointer to the created symbol or NULL on failure
+ */
+symbol_t *symbol_create(char data, size_t freq)
+{
+	symbol_t *s;				/* point to symbol structure */
+
+	s = malloc(sizeof(*s));		/* allocate memory for symbol */
+	if (s == NULL)
+		return (NULL);
+
+	s->data = data;				/* set character data */
+	s->freq = freq;				/* set frequency */
+
+	if (s->freq == 0)			/* check if frequency is zero */
+	{
+		free(s);				/* free memory if so */
+		return (NULL);
+	}
+
+	return (s);					/* return pointer to symbol */
+}
