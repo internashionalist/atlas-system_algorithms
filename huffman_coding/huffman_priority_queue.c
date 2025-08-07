de5f45b6b6e5721edfa78e29d7b844745d42f416
Nash Thames <taylor.thames@atlasschool.com>
2025-08-07 09:40:23 -0500

    betty

--- huffman_coding/huffman_priority_queue.c
+++ huffman_coding/huffman_priority_queue.c
@@ -11,45 +11,45 @@
  */
 static int compare_freq(void *p1, void *p2)
 {
-    binary_tree_node_t *n1, *n2;		/* pointers to nodes */
-    symbol_t *s1, *s2;					/* pointers to symbols */
-    size_t f1, f2;						/* frequencies of symbols */
+	binary_tree_node_t *n1, *n2;		/* pointers to nodes */
+	symbol_t *s1, *s2;					/* pointers to symbols */
+	size_t f1, f2;						/* frequencies of symbols */
 
-    n1 = (binary_tree_node_t *)p1;		/* cast pointers to nodes */
-    n2 = (binary_tree_node_t *)p2;
+	n1 = (binary_tree_node_t *)p1;		/* cast pointers to nodes */
+	n2 = (binary_tree_node_t *)p2;
 
-    if (!n1 && !n2)
-        return (0);
-    if (!n1)
-        return (-1);
-    if (!n2)
-        return (1);
+	if (!n1 && !n2)
+		return (0);
+	if (!n1)
+		return (-1);
+	if (!n2)
+		return (1);
 
-    s1 = (symbol_t *)n1->data;			/* cast node data to symbols */
-    s2 = (symbol_t *)n2->data;
-    f1 = s1 ? s1->freq : 0;				/* get frequencies */
-    f2 = s2 ? s2->freq : 0;
+	s1 = (symbol_t *)n1->data;			/* cast node data to symbols */
+	s2 = (symbol_t *)n2->data;
+	f1 = s1 ? s1->freq : 0;				/* get frequencies */
+	f2 = s2 ? s2->freq : 0;
 
-    if (f1 < f2)						/* compare frequencies */
-        return (-1);
-    else if (f1 > f2)
-        return (1);
+	if (f1 < f2)						/* compare frequencies */
+		return (-1);
+	else if (f1 > f2)
+		return (1);
 
-    if (s1 && s2)						/* tie-break by character */
-    {
-        int c1 = (unsigned char)s1->data;
-        int c2 = (unsigned char)s2->data;
-        if (c1 < c2)
-            return (-1);
-        else if (c1 > c2)
-            return (1);
-    }
-    else if (!s1 && s2)					/* NULL = > */
-        return (-1);
-    else if (s1 && !s2)
-        return (1);
+	if (s1 && s2)						/* tie-break by character */
+	{
+		int c1 = (unsigned char)s1->data;
+		int c2 = (unsigned char)s2->data;
+		if (c1 < c2)
+			return (-1);
+		else if (c1 > c2)
+			return (1);
+	}
+	else if (!s1 && s2)					/* NULL = > */
+		return (-1);
+	else if (s1 && !s2)
+		return (1);
 
-    return (0);
+	return (0);
 }
 
 /**
