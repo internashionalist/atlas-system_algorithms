15511ea337901ebe94c4d72c000220757b2fd1a5
Nash Thames <taylor.thames@atlasschool.com>
2025-08-08 06:01:22 -0500

    base files

---
+++ pathfinding/pathfinding.h
@@ -0,0 +1,39 @@
+#ifndef PATHFINDING_H
+#define PATHFINDING_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "queues.h"
+#include "graphs.h"
+
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+	int x;
+	int y;
+} point_t;
+
+
+/* PROTOTYPES */
+
+/* TASK 0 */
+queue_t *backtracking_array(
+	char **map, int rows, int cols, point_t const *start,
+	point_t const *target);
+
+/* TASK 1 */
+queue_t *backtracking_graph(
+	graph_t *graph, vertex_t const *start, vertex_t const *target);
+
+/* TASK 2 */
+queue_t *dijkstra_graph(
+	graph_t *graph, vertex_t const *start, vertex_t const *target);
+
+#endif // PATHFINDING_H
